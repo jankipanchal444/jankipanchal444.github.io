685535d41c804e2d67beb62dd4bc233cf8e7a121
Darsh Mistry <1111darsh@gmail.com>
2021-03-02 10:16:41 +0530

    Change in feedbacks

--- js/main.js
+++ js/main.js
@@ -115,3 +115,21 @@
 	});
 
 })(jQuery);
+
+
+
+function readmore(dots1, more1, but ) {
+  var dots = document.getElementById(dots1);
+  var moreText = document.getElementById(more1);
+  var btnText = document.getElementById(but)
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
